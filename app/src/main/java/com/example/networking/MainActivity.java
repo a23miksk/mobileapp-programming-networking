5edad8f527aabb994c700f6e34849dae1f45e854
tjugosex <mikael.skaremyr.te1@hotmail.com>
2024-04-19 12:16:30 +0200

    Got JSON to show on recycler view

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -8,47 +8,65 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
+    ArrayList<Mountain> items = new ArrayList<>();
 
+    private RecyclerViewAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new JsonFile(this, this).execute(JSON_FILE);
-        ArrayList<Mountain> items = new ArrayList<>(Arrays.asList(
-                new Mountain("Matterhorn"),
-                new Mountain("Mont Blanc"),
-                new Mountain("Denali")
-        ));
 
-
-
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
+        adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
             @Override
             public void onClick(Mountain item) {
                 Toast.makeText(MainActivity.this, item.getName(), Toast.LENGTH_SHORT).show();
             }
         });
-
         RecyclerView view = findViewById(R.id.recycler_view);
         view.setLayoutManager(new LinearLayoutManager(this));
         view.setAdapter(adapter);
 
-        String url = "https://mobprog.webug.se/json-api?login=brom";
-        new JsonTask(this).execute(url);
+
+        new JsonTask(this).execute(JSON_URL );
     }
 
     @Override
     public void onPostExecute(String json) {
+        try {
+            JSONArray jsonArray = new JSONArray(json);
+            ArrayList<Mountain> newMountains = new ArrayList<>();
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String name = jsonObject.optString("name", "Unknown");
+                String location = jsonObject.optString("location", "No location provided");
+                int size = jsonObject.optInt("size", 0); // size as height
+
+                Mountain mountain = new Mountain(name, location, size);
+                newMountains.add(mountain);
+            }
+
+
+            items.addAll(newMountains);
+            adapter.notifyDataSetChanged();
+
+        } catch (JSONException e) {
+            Log.e("brom", "E:" + e.getMessage());
+        }
         Log.d("MainActivity", json);
     }
 
